836d33580a30bb1d58c65c93c6e83a0932aca96f
Victor Pazaran <victor_hals@hotmail.com>
2023-03-30 19:55:22 -0600

    adding tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
    return (
       <main className={styles.main}>
-         <h1>Hello world! - initial</h1>
+         <h1 className='text-gray-400 text-5xl font-bold'>Hello world! - initial</h1>
       </main>
    )
 }
