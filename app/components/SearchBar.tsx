0b26c65bee90300dec96ae7b818b59c263ee16e2
Victor Pazaran <victor_hals@hotmail.com>
2023-04-15 13:45:07 -0500

    fetching the restaurants from the DB in the search page

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -7,7 +7,7 @@ function SearchBar() {
    const router = useRouter()
    const [location, setLocation] = useState("")
 
-   const handleSearchButton: MouseEventHandler<HTMLButtonElement> = (e) => {
+   const handleSearchButton = () => {
       if (location === "") {
          return
       }
@@ -27,6 +27,7 @@ function SearchBar() {
             placeholder="State, city or town"
             value={location}
             onChange={handleSearchChange}
+            onKeyDown={(e) => {if (e.key === "Enter") {handleSearchButton()}}}
          />
          <button className="rounded bg-red-600 px-9 py-2 text-white"
             onClick={handleSearchButton}>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,67 @@
 import SearchHeader from "./components/SearchHeader"
-import SearchSideBar from "./components/SearchSideBar"
 import SearchRestaurantCard from "./components/SearchRestaurantCard"
+import SearchSideBar from "./components/SearchSideBar"
 
-function Search() {
+import { Cuisine, Location, PRICE, PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export interface RestaurantSearchCardType {
+      id: number
+      name: string
+      main_image: string
+      price: PRICE
+      cuisine: Cuisine
+      slug: string
+      location: Location
+}
+
+const fetchRestaurantsByCity = async (cityName: string | undefined): Promise<RestaurantSearchCardType[]> => {
+   cityName = cityName?.trim().toLowerCase()
+   if (!cityName || cityName === "") {
+      return []
+   }
+
+   let restaurants = await prisma.restaurant.findMany({
+      where: {
+         location: {
+            name: {
+               equals: cityName
+            }
+         }
+      },
+      select: {
+         id: true,
+         name: true,
+         main_image: true,
+         price: true,
+         cuisine: true,
+         slug: true,
+         location: true
+      }
+   })
+
+   return restaurants
+}
+async function Search({searchParams: {city}}: {searchParams: {city?:string}}) {
+   const restaurantsFound = await fetchRestaurantsByCity(city)
+
+
    return (
       <>
          <SearchHeader />
          <div className="flex py-4 m-auto w-2/3 justify-between items-start">
             <SearchSideBar />
             <div className="w-5/6">
-               <SearchRestaurantCard />
+               {restaurantsFound.length > 0 ?
+                  restaurantsFound.map(restaurant => (
+                     <SearchRestaurantCard key={restaurant.id} restaurant={restaurant}/>
+                  ))
+                  :
+                  <div className="flex w-full justify-center p-4">
+                     <h2 className="text-3xl font-bold">No restaurants found :(</h2>
+                  </div>
+               }
             </div>
          </div>
       </>
