fffa591905a5d006e0e6968349c5610951b6d0a2
Victor Pazaran <victor_hals@hotmail.com>
2023-06-13 10:47:29 -0500

    adding our auth context to use it globaly in our application

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React, { useState, createContext } from "react"
+
+interface User {
+   id: number
+   firstName: string
+   lastName: string
+   email: string
+   city: string
+   phone: string
+}
+
+interface AuthState {
+   loading: boolean
+   data: User | null
+   error: string | null
+}
+interface AuthStateSetter extends AuthState{
+   setAuthState: React.Dispatch<React.SetStateAction<AuthState>>
+}
+const AuthenticationContext = createContext<AuthStateSetter>({
+   loading: false,
+   data: null,
+   error: null,
+   setAuthState: () => {}
+})
+
+export default function AuthContext({children}: {children: React.ReactNode}) {
+   const [authState, setAuthState] = useState<AuthState>({
+      loading: false,
+      data: null,
+      error: null,
+   })
+
+   return (
+      <AuthenticationContext.Provider value={{...authState, setAuthState}}>
+         {children}
+      </AuthenticationContext.Provider>
+   )
+}
